cd4fc6009bacd7ea17bb7c80acb0ce45c7fe87d6
Danialprabukj <116823631+Danialprabukj@users.noreply.github.com>
2023-04-10 17:59:47 +0530

    3 files modified

--- src/app/Employee/employee-details/employee-details.component.ts
+++ src/app/Employee/employee-details/employee-details.component.ts
@@ -6,7 +6,7 @@ import { EmployeeService } from '../../employee.service';
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
-  styleUrls: ['./employee-details.component.css']
+    styleUrls: ['./employee-details.component.css']
 })
 export class EmployeeDetailsComponent implements OnInit {
 
--- src/app/Employee/employee.module.ts
+++ src/app/Employee/employee.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmployeesRoutingModule } from '../Employee/employees-routing.module';
 import { EmployeeListComponent } from '../Employee/employee-list/employee-list.component';
-import { UpdateEmployeeComponent } from '../Employee/update-employee/update-employee.component';
-import { EmployeeDetailsComponent } from '../Employee/employee-details/employee-details.component';
-import { CreateEmployeeComponent } from '../Employee/create-employee/create-employee.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
